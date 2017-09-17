29cdee37fb307bdea4185036a3a56f1b6adf6a0d
SoulJaZhao <superzhaolong@126.com>
2017-09-17 12:27:39 +0800

    应用列表

--- SDPPushClient/Modules/Application/Controller/SDPAppListViewController.swift
+++ SDPPushClient/Modules/Application/Controller/SDPAppListViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class SDPAppListViewController: SDPBaseViewController {
-
+class SDPAppListViewController: SDPBaseViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    /* tableView */
+    var tableView:UITableView = UITableView()
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // 设置Nav
+        self.initNav()
+        // 设置子视图
+        self.initSubviews()
+    }
+    
+    //MARK:设置Nav
+    func initNav() {
+        self.title = "应用列表"
+    }
+    
+    //MARK:设置子视图
+    func initSubviews() {
+        tableView = UITableView(frame: self.view.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: cellId)
+        self.view.addSubview(tableView)
+    }
+    
+    //MARK:UITableViewDataSource, UITableViewDelegate
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellId) else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = "haha"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
