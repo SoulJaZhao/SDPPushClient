5ccaf729ba3e58f55559c55738da34bf7286626d
zhaolong01 <zhaolong01@shengpay.com>
2017-10-20 14:15:49 +0800

    完成退出登录

--- SDPPushClient/Modules/Setting/Controller/SDPSettingViewController.swift
+++ SDPPushClient/Modules/Setting/Controller/SDPSettingViewController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
-class SDPSettingViewController: SDPBaseViewController {
+class SDPSettingViewController: SDPBaseViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // 设置Nav
+        self.initNav()
+        // 设置子视图
+        self.initSubviews()
+    }
+    
+    //MARK:设置Nav
+    func initNav() {
+        self.title = "设置"
+    }
+    
+    //MARK:设置子视图
+    func initSubviews() {
+        let tableView = UITableView(frame: self.view.bounds, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.view.addSubview(tableView)
+    }
+    
+    //MARK:UITableViewDataSource, UITableViewDelegate
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kCellId = "cellId"
+        
+        var cell:UITableViewCell
+        
+        if let tempCell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: kCellId) {
+            cell = tempCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: kCellId)
+        }
+        
+        cell.textLabel?.text = "退出登录"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            
+            self.tabBarController?.selectedIndex = 0
+            
+            UserDefaults.standard.removeObject(forKey: kSDPTouchIDSupportType)
+            
+            let loginVC = SDPLoginViewController()
+            let nav:SDPNavigationController = SDPNavigationController(rootViewController: loginVC)
+            self.present(nav, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
